468a96a82110db777f4e44c7bc557f8aa8ed49f4
unknown <ajay.kotnalajpr@gmail.com>
2016-04-09 19:49:51 +0530

    serverless commit

---
+++ Serverless4Demo/DemoProject/RestApi/lib/dynamodb.js
@@ -0,0 +1,27 @@
+﻿var dynamodb = module.exports;
+
+var projectName = process.env.SERVERLESS_PROJECT_NAME;
+var stage = process.env.SERVERLESS_STAGE;
+
+var projectTable = projectName + '-project-' + stage;
+
+var byId = 'by-id';
+var byUserId = 'by-userid';
+
+dynamodb.byId = byId;
+dynamodb.byUserId = byUserId;
+
+dynamodb.projects = vogels.define('Project', {
+    hashKey: 'index',
+    timestamps: true,
+    schema: {
+        id: vogels.types.uuid(),
+        index: joi.string(),
+        name: joi.string(),
+        description: joi.string()
+    },
+    indexes: [
+        { hashKey: 'id', name: byId, type: 'global' }
+    ],
+    tableName: projectTable
+});
---
+++ Serverless4Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serverless4Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Serverless4Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dd90f96-8032-4e27-80f2-a91aa9d02e6a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
